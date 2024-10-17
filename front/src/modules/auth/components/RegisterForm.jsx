3b34a3e8a20885bd4398a39210d4faa05b67d39d
nazariegode <dnazariego@gmail.com>
2024-10-17 15:38:29 -0300

    style: register form stylized

--- front/src/modules/auth/components/RegisterForm.jsx
+++ front/src/modules/auth/components/RegisterForm.jsx
@@ -1,5 +1,9 @@
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import lines from "../../../assets/lines.svg";
+import logo from "../../../assets/logotipo.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
 
 const schema = Yup.object().shape({
   name: Yup.string().required("Por favor ingresa tu nombre completo."),
@@ -22,42 +26,129 @@ export const RegisterForm = () => {
         password: "",
       },
       validationSchema: schema,
+      onSubmit: (values) => {
+        console.log("Form values", values);
+      },
     });
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Nombre completo"
-        name="name"
-        value={values.name}
-        onChange={handleChange}
-        onBlur={handleBlur}
-      />
-      {touched.name && errors.name && <span>{errors.name}</span>}
+    <div className="flex h-screen">
+      {/* Imagen a la izquierda */}
+      <div className="w-1/2 flex-shrink-0h-72 bg-gradient-to-b from-teal-600 to-cyan-950 static">
+        <div className="absolute bottom-0 left-0">
+          <img
+            src={lines}
+            alt="Registro"
+            className="w-1/2 h-full object-cover "
+          />
+        </div>
+      </div>
 
-      <input
-        type="email"
-        placeholder="Correo electrónico"
-        name="email"
-        value={values.email}
-        onChange={handleChange}
-        onBlur={handleBlur}
-      />
-      {touched.email && errors.email && <span>{errors.email}</span>}
+      {/* Formulario a la derecha */}
+      <div className="w-1/2 flex justify-center items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="max-w-md w-full bg-white p-6 space-y-4"
+        >
+          {/* Logotipo */}
+          <div>
+            <img src={logo} alt="logo" className="w-1/2 h-full object-cover" />
+          </div>
+          {/* Titulo y sub */}
+          <div>
+            <h3 className="text-2xl font-bold text-gray-800">Regístrate</h3>
+            <p className="text-sm text-gray-500">Crea tu cuenta</p>
+          </div>
 
-      <input
-        type={"password"}
-        placeholder="password"
-        name="password"
-        value={values.password}
-        onChange={handleChange}
-        onBlur={handleBlur}
-      />
-      {touched.password && errors.password && <span>{errors.password}</span>}
+          {/* Input nombre */}
+          <div className="relative">
+            <FontAwesomeIcon
+              icon={faUser}
+              className={`absolute left-5 top-6 transform -translate-y-1/2 transition-all duration-200 
+              ${
+                touched.name && values.name ? "text-cyan-700" : "text-gray-300"
+              }`}
+            />
+            <input
+              type="text"
+              placeholder="Nombre completo"
+              name="name"
+              value={values.name}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className={`w-full p-3 pl-11 border border-gray-200 rounded-3xl 
+              focus:outline-none focus:ring-1 focus:ring-gray-400 transition-all duration-200`}
+            />
+            {touched.name && errors.name && (
+              <span className="text-red-400 text-sm">{errors.name}</span>
+            )}
+          </div>
 
-      <button type="submit">Registrarme</button>
-      <br />
-    </form>
+          {/* Input correo */}
+          <div className="relative">
+            <FontAwesomeIcon
+              icon={faEnvelope}
+              className={`absolute left-5 top-6 transform -translate-y-1/2 transition-all duration-200 
+              ${
+                touched.email && values.email
+                  ? "text-cyan-700"
+                  : "text-gray-300"
+              }`}
+            />
+            <input
+              type="email"
+              placeholder="Correo electrónico"
+              name="email"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className={`w-full p-3 pl-11 border border-gray-200 rounded-3xl 
+              focus:outline-none focus:ring-1 focus:ring-gray-400 transition-all duration-200`}
+            />
+            {touched.email && errors.email && (
+              <span className="text-red-400 text-sm">{errors.email}</span>
+            )}
+          </div>
+
+          {/* Input contraseña */}
+          <div className="relative">
+            <FontAwesomeIcon
+              icon={faLock}
+              className={`absolute left-5 top-6 transform -translate-y-1/2 transition-all duration-200 
+              ${
+                touched.password && values.password
+                  ? "text-cyan-700"
+                  : "text-gray-300"
+              }`}
+            />
+            <input
+              type="password"
+              placeholder="Contraseña"
+              name="password"
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className={`w-full p-3 pl-11 border border-gray-200 rounded-3xl 
+              focus:outline-none focus:ring-1 focus:ring-gray-400 transition-all duration-200`}
+            />
+            {touched.password && errors.password && (
+              <span className="text-red-400 text-sm">{errors.password}</span>
+            )}
+          </div>
+
+          <button
+            type="submit"
+            className="w-1/2 bg-cyan-700 text-white py-3 rounded-3xl hover:bg-cyan-950 transition duration-300"
+          >
+            Registrarme
+          </button>
+          <div>
+            <a className="text-sm text-gray-400" href="#">
+              ¿Ya tienes una cuenta?
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
