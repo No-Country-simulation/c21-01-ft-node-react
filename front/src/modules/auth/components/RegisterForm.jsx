7d7da41e249602c27fd98b555d45d2189a629f9c
NazarenoDersho <nazarenodersho@gmail.com>
2024-10-16 21:19:50 -0300

    feat(modules): Register form added

---
+++ front/src/modules/auth/components/RegisterForm.jsx
@@ -0,0 +1,68 @@
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+// Esquema de validaciones
+const schema = Yup.object().shape({
+  name: Yup.string().required("Por favor ingresa tu nombre completo."),
+  email: Yup.string()
+    .email("Por favor ingresa un correo electrónico válido")
+    .required("El correo electrónico es obligatorio."),
+  password: Yup.string()
+    .min(8, "La contraseña debe tener al menos 8 caracteres.")
+    .matches(/[A-Z]/, "La contraseña debe tener al menos una mayúscula.")
+    .matches(/\d/, "La contraseña debe tener al menos un número.")
+    .required("La contraseña es obligatoria."),
+});
+
+export const RegisterForm = () => {
+  // Función para el envío del formulario
+//   const submitForm = (values) => {};
+
+  // Implementación de Formik
+  const { handleSubmit, handleChange, handleBlur, errors, touched, values } =
+    useFormik({
+      initialValues: {
+        name: "",
+        email: "",
+        password: "",
+      },
+    //   onSubmit: submitForm,
+      validationSchema: schema,
+    });
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Nombre completo"
+        name="name"
+        value={values.name}
+        onChange={handleChange}
+        onBlur={handleBlur}
+      />
+      {touched.name && errors.name && <span>{errors.name}</span>}
+
+      <input
+        type="email"
+        placeholder="Correo electrónico"
+        name="email"
+        value={values.email}
+        onChange={handleChange}
+        onBlur={handleBlur}
+      />
+      {touched.email && errors.email && <span>{errors.email}</span>}
+
+      <input
+        type={"password"}
+        placeholder="password"
+        name="password"
+        value={values.password}
+        onChange={handleChange}
+        onBlur={handleBlur}
+      />
+      {touched.password && errors.password && <span>{errors.password}</span>}
+      <button type="submit">Registrarme</button>
+      <br />
+    </form>
+  );
+};
