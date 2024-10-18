1c4f73fc666da706c9f011b3d4279ed300328209
Nachit0C <nachociccone@gmail.com>
2024-10-18 14:22:06 -0300

    docs: added comments to user controllers

--- back/src/controllers/usercontroller.js
+++ back/src/controllers/usercontroller.js
@@ -1,10 +1,12 @@
 import { pool } from '../middlewares/pool.js';
 
+// createUser toma los datos del body y los inserta en la tabla usuario de la base de datos
+// envía como respuesta un mensaje de éxito junto con la lista de usuarios o un mensaje de error junto con el mismo
 export const createUser = async (req, res) => { 
     try{
         const data = req.body;
         if (!validarDatos(data.email)) return res.status(400).send('El mail ya está registrado');
-        // Chequar nombres de tabla, columnas y del objeto data
+        // **Chequar nombres de tabla, columnas y del objeto data** //
         const result = await pool.query('INSERT INTO usuario (Nombre, Email, Contrasena) VALUES ($1, $2, $3) RETURNING *', [data.nombre, data.email, data.contrasena]);
     
         res.status(201).json({res: result.rows, message: 'Usuario creado correctamente'});
@@ -14,13 +16,15 @@ export const createUser = async (req, res) => {
     }
 }
 
+// validarDatos toma un email y chequea si ya existe en la base de datos
+// devuelve true si no existe y false si ya está registrado
 const validarDatos = async (email) => {
     try{
         const emailCheck = await pool.query('SELECT * FROM usuario WHERE email = $1', [email]);
             if (emailCheck.rows.length > 0) return false;
         return true;
     } catch(err) {
-        console.error('Error validando el correo: ', error);
+        console.error('Error validando el correo: ', err);
         return false;
     }
 }
