7c5bd20751ef1de7b16bbf02db10bbba18511c96
NazarenoDersho <nazarenodersho@gmail.com>
2024-10-21 22:02:02 -0300

    fix: success values returned on each instance of forms validations

--- front/src/modules/auth/services/handleFormSubmit.js
+++ front/src/modules/auth/services/handleFormSubmit.js
@@ -3,6 +3,7 @@ export const handleFormSubmit = (values, formType) => {
     if (formType === "register") {
       localStorage.setItem("user", JSON.stringify(values));
       console.log("Registered user: ", values);
+      return { success: true };
     } else if (formType === "login") {
       const storedUser = JSON.parse(localStorage.getItem("user"));
 
@@ -12,11 +13,14 @@ export const handleFormSubmit = (values, formType) => {
         storedUser.password === values.password
       ) {
         console.log("Login successfull: ", values);
+        return { success: true };
       } else {
         console.error("Incorrect username or password: ");
+        return { success: false, error: "Incorrect username or password" };
       }
     }
   } catch (error) {
     console.error("Error sending form: ", error);
+    return { success: false, error: "An error occurred" };
   }
 };
