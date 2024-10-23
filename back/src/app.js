cc05a0781805733a9ec8b1bd6404e22afd483f11
Nachit0C <nachociccone@gmail.com>
2024-10-23 18:32:26 -0300

    feat: add database, models and app.js

--- back/src/app.js
+++ back/src/app.js
@@ -1,20 +1,13 @@
 import express from 'express';
-import  pool from './middlewares/pool.js';
-
+import { sequelize, initDB } from "./database/db.js";
+import "./database/usersModel.js";
+import "./database/accountsModel.js";
+import "./database/transactionsModel.js";
 const app = express();
+
 const port = process.env.PORT || 3000;
 
-
-app.get('/', async (req, res) => {
-  try{
-    const result = await pool.query('SELECT NOW()')
-    res.send(result.rows[0]);
-    console.log('Conexion exitosa')
-  } catch(error) {
-    console.log(error);
-    res.status(500).send('Server Error');
-  }
-});
+await initDB();
 
 app.listen(port, () => {
   console.log('Server running on port 3000');
