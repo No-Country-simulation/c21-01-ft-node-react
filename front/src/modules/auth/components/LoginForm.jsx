9a9dfc26ef6d5331db995a6186f8694080fd7f74
nazariegode <dnazariego@gmail.com>
2024-10-24 12:10:25 -0300

    feat: creating the popups component and adding it to the forms

--- front/src/modules/auth/components/LoginForm.jsx
+++ front/src/modules/auth/components/LoginForm.jsx
@@ -4,7 +4,6 @@ import { loginSchema } from "../schemas/validationSchemas";
 import { FormInput } from "./FormInput";
 import { Popup } from "./Popup";
 import { Link } from "react-router-dom";
-
 import logo from "../../../assets/logotipo.svg";
 import { faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
 
@@ -29,70 +28,70 @@ export const LoginForm = () => {
   );
 
   return (
-    <div className="w-login-form flex justify-center items-center">
-      <form
-        onSubmit={handleSubmit}
-        noValidate
-        className="max-w-md w-form-content bg-white space-y-4"
-      >
-        <div>
-          <img src={logo} alt="logo" className="w-2/3 h-full object-cover" />
-        </div>
+    <div className="w-login-form flex flex-col justify-center items-center">
+      <div className="mb-4">
+        <img src={logo} alt="logo" className="w-2/3 mb-4 h-full object-cover" />
+      </div>
 
-        <div>
-          <h3 className="text-heading font-bold text-text-primary">
-            Bienvenido
-          </h3>
-          <p className="text-body text-text-secondary">Accede a tu Cuenta</p>
-        </div>
+      {!showPopup ? (
+        <form
+          onSubmit={handleSubmit}
+          noValidate
+          className="max-w-md w-form-content bg-white space-y-4"
+        >
+          <div>
+            <h3 className="text-heading font-bold text-text-primary">
+              Bienvenido
+            </h3>
+            <p className="text-body text-text-secondary">Accede a tu Cuenta</p>
+          </div>
 
-        <FormInput
-          icon={faEnvelope}
-          type="email"
-          placeholder="Correo electrónico"
-          name="email"
-          value={values.email}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          touched={touched.email}
-          error={errors.email}
-        />
+          <FormInput
+            icon={faEnvelope}
+            type="email"
+            placeholder="Correo electrónico"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            touched={touched.email}
+            error={errors.email}
+          />
 
-        <FormInput
-          icon={faLock}
-          type="password"
-          placeholder="Contraseña"
-          name="password"
-          value={values.password}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          touched={touched.password}
-          error={errors.password}
-        />
+          <FormInput
+            icon={faLock}
+            type="password"
+            placeholder="Contraseña"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            touched={touched.password}
+            error={errors.password}
+          />
 
-        <div>
-          <a className="text-body text-text-link" href="#">
-            ¿Has olvidado tu contraseña?
-          </a>
-        </div>
+          <div>
+            <a className="text-body text-text-link" href="#">
+              ¿Has olvidado tu contraseña?
+            </a>
+          </div>
 
-        <div className="w-full flex justify-between">
-          <button
-            type="submit"
-            className="w-button-large bg-primary text-white py-buttonPadding rounded-3xl hover:bg-primary-dark transition duration-300"
-          >
-            Ingresar
-          </button>
-          <Link
-            to="/register"
-            className="w-button-small border border-primary text-primary mx-2 py-buttonPadding rounded-3xl hover:bg-primary-dark hover:text-white hover:border-primary-dark transition duration-300"
-          >
-            Regístrate
-          </Link>
-        </div>
-      </form>
-
-      {showPopup && (
+          <div className="w-full flex justify-between">
+            <button
+              type="submit"
+              className="w-button-large bg-primary text-white py-buttonPadding rounded-3xl hover:bg-primary-dark transition duration-300"
+            >
+              Ingresar
+            </button>
+            <Link
+              to="/register"
+              className="w-button-small flex justify-center items-center text-center border border-primary text-primary mx-2 py-buttonPadding rounded-3xl hover:bg-primary-dark hover:text-white hover:border-primary-dark transition duration-300"
+            >
+              Regístrate
+            </Link>
+          </div>
+        </form>
+      ) : (
         <Popup
           success={formResult.success}
           formType="login"
