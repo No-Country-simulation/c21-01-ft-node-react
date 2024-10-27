a43e0f2f68f32d961439ac0353eab29f92eda732
Nachit0C <nachociccone@gmail.com>
2024-10-27 16:37:34 -0300

    fix: console logs removed

--- back/src/controllers/usercontroller.js
+++ back/src/controllers/usercontroller.js
@@ -5,7 +5,6 @@ import bcypt from 'bcrypt';
 export const createUser = async (req, res) => { 
     try{
         const {Name, Email, Password} = req.body;
-        console.log("Name:", Name, "Email:", Email, "Password:", Password);
         const emailExists = await validateEmail(Email);
         if (emailExists) return res.status(400).send('Email already registered');
 
@@ -16,11 +15,9 @@ export const createUser = async (req, res) => {
             Email,
             Password: hashedPassword
         });
-        console.log(newUser);
     
         res.status(201).json({res: newUser, message: 'User successfully created'});
     } catch(err) {
-        console.error(err);
         res.status(500).json({ message: 'Server error', error: err });
     }
 }
