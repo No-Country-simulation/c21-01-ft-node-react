a382d0802be4bb7ff453b1d378fcaa70f6076ea9
NazarenoDersho <nazarenodersho@gmail.com>
2024-10-27 09:18:41 -0300

    fix: ProtectedRoute component removed due to obstructing user login

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,15 +4,12 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import { ProtectedRoute } from "./core/utils/ProtectedRoute";
 
 import { RegisterPage } from "./modules/auth/pages/RegisterPage";
 import { LoginPage } from "./modules/auth/pages/LoginPage";
 import { DashboardPage } from "./modules/dashboard/pages/DashboardPage";
 
 function App() {
-  const user = JSON.parse(localStorage.getItem("user"));
-
   const routes = [
     {
       path: "/login",
@@ -24,11 +21,7 @@ function App() {
     },
     {
       path: "/dashboard",
-      element: (
-        <ProtectedRoute canNavigate={user} redirectPath="/login">
-          <DashboardPage />
-        </ProtectedRoute>
-      ),
+      element: <DashboardPage />,
     },
     {
       path: "*",
