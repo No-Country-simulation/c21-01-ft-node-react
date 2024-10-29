a79a2ed54581e42ad698cc34c1a568c110bd7a3a
NazarenoDersho <nazarenodersho@gmail.com>
2024-10-29 18:27:01 -0300

    style: welcome title deleted

--- front/src/modules/dashboard/components/Finance.jsx
+++ front/src/modules/dashboard/components/Finance.jsx
@@ -1,15 +1,8 @@
-import { getUserName } from "../utils/getUserName";
 import { ToggleSection } from "./ToggleSection";
 
 export const Finance = () => {
-  const userName = getUserName();
   return (
-    <div className="w-full px-4">
-      <h1 className="text-2xl font-bold">{`¡Bienvenido, ${userName}!`}</h1>
-      <p className="py-0 mb-2">
-        Coloca tus ingresos y egresos para controlar tus finanzas.
-      </p>
-
+    <div className="w-full p-4">
       <ToggleSection />
     </div>
   );
--- front/src/modules/dashboard/components/Graphics.jsx
+++ front/src/modules/dashboard/components/Graphics.jsx
@@ -1,28 +1,18 @@
-import { getUserName } from "../utils/getUserName";
-
 export const Graphics = () => {
-  const userName = getUserName();
-
   return (
-    <div className="w-full px-4 ">
-      <h1 className="text-2xl font-bold ">{`¡Bienvenido, ${userName}!`}</h1>
-      <p className="py-0 mb-2">
-        Tu panel financiero está listo para para simplificar la gestión de tus
-        cuentas
-      </p>
-
-      <div className="bg-slate-200 h-64 w-full mb-2 rounded-2xl shadow-md shadow-gray-300">
+    <div className="w-full h-full relative p-4">
+      <div className="bg-slate-200 h-1/2 w-full mb-2 rounded-2xl shadow-md shadow-gray-300">
         <p className="text-center pt-24">Gráfico Grande</p>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
-        <div className="bg-slate-200 h-56 rounded-2xl shadow-md shadow-gray-300">
+      <div className="grid grid-cols-3 gap-2 h-1/2">
+        <div className="bg-slate-200 h-full rounded-2xl shadow-md shadow-gray-300">
           <p className="text-center pt-16">Gráfico Pequeño 1</p>
         </div>
-        <div className="bg-slate-200 h-56 rounded-2xl shadow-md shadow-gray-300">
+        <div className="bg-slate-200 h-full rounded-2xl shadow-md shadow-gray-300">
           <p className="text-center pt-16">Gráfico Pequeño 2</p>
         </div>
-        <div className="bg-slate-200 h-56 rounded-2xl shadow-md shadow-gray-300">
+        <div className="bg-slate-200 h-full rounded-2xl shadow-md shadow-gray-300">
           <p className="text-center pt-16">Gráfico Pequeño 3</p>
         </div>
       </div>
