e424a1ef3028b1c52c906f79bd2393e74e92e9f0
brian-h2 <brianheredia200309@gmail.com>
2024-10-29 22:00:57 -0300

    Feat conexion with api in register and login

--- front/src/modules/auth/hooks/useCustomFormik.js
+++ front/src/modules/auth/hooks/useCustomFormik.js
@@ -1,7 +1,7 @@
 import { useFormik } from "formik";
 import { handleFormSubmit } from "../services/handleFormSubmit";
-import { useState } from "react";
-
+import {useState } from "react";
+import axios from 'axios'
 export const useCustomFormik = (initialValues, validationSchema, formType) => {
   const [showPopup, setShowPopup] = useState(false);
   const [formResult, setFormResult] = useState({ success: false });
@@ -12,12 +12,55 @@ export const useCustomFormik = (initialValues, validationSchema, formType) => {
     initialValues,
     validationSchema,
     onSubmit: (values) => {
+      conexionApi(values,formType)
       const result = handleFormSubmit(values, formType);
+      
+
 
       setFormResult({ success: result.success });
       setShowPopup(true);
     },
   });
+
+  const conexionApi = async (values,formType) => {
+    if(formType == 'login') {
+      try {
+        const res = await axios.post('http://localhost:3000/users/login', {
+          email: values.email,
+          password: values.password
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+      
+        )
+      alert(res.message) 
+      } catch (error) {
+        console.log(error.message)
+      }
+    } else {
+      try {
+        const res = await axios.post('http://localhost:3000/users/register', {
+          name: values.name,
+          email: values.email,
+          password: values.password
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+      
+        )
+      alert(res.message) 
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+    }
+
 
   return { ...formik, showPopup, formResult, closePopup };
 };
