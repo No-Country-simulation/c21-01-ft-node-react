5d02968b95d7aeefcb884de03b5bab97631614e6
luiggy <luiggylml@gmail.com>
2024-10-31 20:04:59 -0500

    chore: add config deploy

--- back/src/database/db.js
+++ back/src/database/db.js
@@ -10,6 +10,12 @@ export const sequelize = new Sequelize({
   host: process.env.PG_HOST,
   port: process.env.PG_PORT ?? 5432,
   dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 export const initDB = async () => {
